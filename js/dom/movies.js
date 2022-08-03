f19e1041bd80ade2bced4ee4d1482d7fd2da5aef
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-08-03 15:39:24 +0300

    make search functionality in movie.html

--- js/dom/movies.js
+++ js/dom/movies.js
@@ -1,3 +1,5 @@
+const cardsContainer = document.querySelector(".cards-container");
+
 function classNameRating(rate) {
   return rate >= 7 ? "green" : rate >= 5 ? "orange" : "red";
 }
@@ -9,7 +11,6 @@ const renderMovies = (res) => {
 };
 
 const renderMovieCard = (movie) => {
-  const cardsContainer = document.querySelector(".cards-container");
   let movieCard = document.createElement("div");
   movieCard.id = movie.id;
   movieCard.classList.add("movie");
@@ -34,3 +35,92 @@ const renderMovieCard = (movie) => {
 };
 
 fetch("https://yts.mx/api/v2/list_movies.json?limit=50", renderMovies);
+// --------------------------------------------------------------------------------------
+const testArray = [
+  {
+    id: 44,
+    title: "Any Thing",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 55,
+    title: "Kung Fu Ghost",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 66,
+    title: "Any Thing",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 99,
+    title: "Rider on a Dead Horse",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 22,
+    title: "nient Alien Origins ",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 11,
+    title: "Any Thing",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 455,
+    title: "Softie",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }, {
+    id: 112,
+    title: "Albatross",
+    medium_cover_image:
+      "https://yts.mx/assets/images/movies/minions_the_rise_of_gru_2022/medium-cover.jpg",
+    rating: 9,
+  }
+];
+
+
+/// ----> cases : empty string ----> all data from array in local storage
+/// ----> cases : not found (Empty array from search function ) ----> display not found to user
+/// ----> cases : found result ------> display result
+
+// information to store in local storage  [ id | img url | title | rate ]
+// const notFound = document.querySelector(".not-found");
+
+const inputSearch = document.querySelector("#search");
+inputSearch.addEventListener('keyup', (e)=>{
+    let string = e.target.value.toLowerCase() ;
+    if(string === ""){
+        cardsContainer.innerHTML = "";
+        fetch("https://yts.mx/api/v2/list_movies.json?limit=50", renderMovies);
+    }else {
+    cardsContainer.innerHTML = "";
+    searchDom(search.LocalSearch(testArray,string)) ;
+    }
+});
+
+function searchDom(resultArr){
+   if(resultArr.length === 0){
+    cardsContainer.innerHTML = "";
+    let emptyParagraph = document.createElement("div");
+    emptyParagraph.textContent = "There Is No Result 🥲 ";
+    emptyParagraph.classList.add("not-found")
+    cardsContainer.appendChild(emptyParagraph);
+   }
+   else {
+    cardsContainer.innerHTML = "";
+    resultArr.forEach((ele) => {
+        renderMovieCard(ele);
+      });
+   }
+}
+
